2697b828d3ab7b5f134e92bc48104fb91959e1be
Abubeker <Abubekera87@gmail.com>
2024-12-13 21:55:42 +0300

    remover unused import

--- Vehicle_management/src/hooks/useCreateVehicle.ts
+++ Vehicle_management/src/hooks/useCreateVehicle.ts
@@ -1,9 +1,5 @@
 import axiosClient from "@/lib/axiosClient";
-import {
-  QueryClient,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const useCreateVehicles = () => {
   const queryclient = useQueryClient();
